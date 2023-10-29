32dc129467d719ee1935f7ac9c171ca31b074e0c
ezbw69 <lipfaris00@gmail.com>
2023-10-29 07:25:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -78,21 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/0f9612b2f385dcd9dbc5e.png"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
+STATS_IMG_URL = "https://telegra.ph//file/7c9c1831099058beca9e9.png"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
+STREAM_IMG_URL = "https://telegra.ph/file/ba26163dc4327af3bad7f.png"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/5f4765b2273c31dc59b3d.png"
+YOUTUBE_IMG_URL = "https://telegra.ph//file/7c9c1831099058beca9e9.png"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/52fd7ea4dc0fe42eac4ee.png"
 
 
 def time_to_seconds(time):
