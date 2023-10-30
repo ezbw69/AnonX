25604ee67bfa7ca06c14ce5e4b623c923331d11e
ezbw69 <lipfaris00@gmail.com>
2023-10-30 07:58:51 +0700

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -21,7 +21,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        LOGGER(__name__).error("Assistant client vars tidak ada, exiting...")
         exit()
     await sudo()
     try:
